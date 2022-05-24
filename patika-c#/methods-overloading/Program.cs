eebf445a4e62199990ae75400c2728555810c4cc
Yasin Anil Bostanci <44286549+yasinanilboss@users.noreply.github.com>
2022-05-24 12:48:50 +0300

    added methods-overloading

---
+++ patika-c#/methods-overloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace methods_overloading // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("başarılı");
+                Console.WriteLine(outSayi);
+
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+            //Metot Aşırı Yükleme-Overloading
+
+
+            int ifade = 999;
+            instance.ekranaYazdir(Convert.ToString(ifade));
+            instance.ekranaYazdir(ifade);
+            //metot imzası 
+            //metotAdi+parametre sayisi + parametre
+            instance.ekranaYazdir("Ozcan", "Acar");
+
+            Console.ReadLine();
+        }
+    }
+    class Metotlar
+    {
+
+        public void Topla(int a, int b, out int toplam)
+        {
+
+            toplam = a + b;
+        }
+        public void ekranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void ekranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void ekranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+
+    }
+}
